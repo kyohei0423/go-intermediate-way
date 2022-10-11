5a36f457e1e1aa7067e792e564b5549381d74a3e
kyohei0423 <kyo199333@gmail.com>
2022-10-11 23:51:12 +0900

    モデルの構造体を定義

---
+++ models/article.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+)
+
+type Article struct {
+	ID          int       `json:"article_id"`
+	Title       string    `json:"title"`
+	Contents    string    `json:"contents"`
+	UserName    string    `json:"user_name"`
+	NiceNum     int       `json:"nice"`
+	CommentList []Comment `json:"comments"`
+	CreatedAt   time.Time `json:"created_at"`
+}
